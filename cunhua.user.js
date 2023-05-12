111ee18a1050ccba02d7cfc317b2427e27c5a4ea
gsons <976955017@qq.com>
2023-05-12 16:10:08 +0800

    init

---
+++ cunhua.user.js
@@ -0,0 +1,58 @@
+// ==UserScript==
+// @name         cunhua
+// @namespace    https://cunhua.click/
+// @version      0.1
+// @description  cunhua
+// @author       You
+// @match        https://cunhua.click/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=cunhua.click
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    async function fetch_real_url(url) {
+        const resp = await fetch(url);
+        const html = await resp.text();
+        let [, jsStr] = /<script.*?>([\s\S]*?)<\/script>/gm.exec(html);
+        const temp = `
+        MuURL='';
+        MuObj={
+            href:'',replace:function(abc){MuURL=abc},
+            assign:function(abc){MuURL=abc},
+        };` ;
+        jsStr = temp + jsStr.replaceAll('location', 'MuObj');
+        //console.log(jsStr);
+        let func = new Function(jsStr);
+        func();
+        MuURL = MuURL ? MuURL : (MuObj.href || MuObj);
+        let [, _dsign] = /_dsign=(.*)/gm.exec(MuURL);
+        const _url = url + '&_dsign=' + _dsign;
+        return _url;
+    }
+
+    async function getImages(url) {
+        //console.log(url);
+        const real_url = await fetch_real_url(url);
+        const resp = await fetch(real_url);
+        const html = await resp.text();
+        var regex = /<img[^>]+id="aimg_\d+"[^>]*>/g;
+        var matches = html.match(regex);
+        return matches.map((v) => { return $(v).attr('src'); });
+        // const urls = $(html).find('.message a>img')
+        //     .map(function () {
+        //         return $(this).attr('src');
+        //     }).get();
+        // return urls;
+    }
+
+    $('.threadlist a.z').each(async function (index, vo) {
+        var url = $(this).attr('href');
+        const links = await getImages('https://' + location.host + '/' + url);
+        console.log(url, index, links);
+        links.slice(0, 4).forEach((link) => {
+            $(this).parent().append('<img style="max-width:140px;max-height:240px;padding:5px;" src="' + link + '">');
+        });
+    });
+})();
---
+++ openai_ws.user.js
@@ -0,0 +1,193 @@
+// ==UserScript==
+// @name          OPENAI-WS
+// @namespace    OPENAI-WS
+// @version        0.1
+// @description  OPENAI-WS
+// @author       gsonhub
+// @match             https://chat.openai.com/?service=*
+// @icon              data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @connect           127.0.0.1
+// @connect           127.0.0.1:55151
+// @connect           chat.openai.com/
+// @grant               GM_setValue
+// @grant               GM_getValue
+// @grant               GM_notification
+// @grant               GM_registerMenuCommand
+// @grant               GM_unregisterMenuCommand
+// @grant               GM.xmlHttpRequest
+
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    async function getAccessToken() {
+        const resp = await fetch('https://chat.openai.com/api/auth/session')
+        if (resp.status === 403) {
+            throw new Error('CLOUDFLARE')
+        }
+        const data = await resp.json();
+        if (!data.accessToken) {
+            throw new Error('UNAUTHORIZED')
+        }
+        return data.accessToken
+    }
+
+    function uuidv4() {
+        var d = new Date().getTime()
+        var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+            var r = (d + Math.random() * 16) % 16 | 0
+            d = Math.floor(d / 16)
+            return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16)
+        })
+        return uuid
+    }
+
+    async function generateAnswer(question, callback) {
+        let accessToken = await getAccessToken();
+        let pdata = {
+            action: 'next',
+            messages: [{
+                role: 'user', id: question.id || uuidv4(),
+                content: { content_type: 'text', parts: [question.content] }
+            }],
+            model: 'text-davinci-002-render',
+            parent_message_id: question.pid || uuidv4(),
+            max_tokens: 4000
+        };
+        if (question.cid) {
+            pdata = { ...pdata, ...{ parent_message_id: question.pid, conversation_id: question.cid } };
+        }
+        console.log(pdata);
+
+        GM.xmlHttpRequest({
+            method: 'POST', url: 'https://chat.openai.com/backend-api/conversation',
+            headers: { 'Content-Type': 'application/json', Authorization: 'Bearer ' + accessToken },
+            responseType: 'stream',
+            data: JSON.stringify(pdata),
+            onloadstart: function (stream) {
+                var reader = stream.response.getReader()
+                reader.read().then(function processText({ done, value }) {
+                    if (done) {
+                        return;
+                    }
+
+                    let responseItem = String.fromCharCode(...Array.from(value));
+
+                    //console.log('responseItem',"start***"+responseItem+"***end");
+                    // if (responseItem.includes('Internal Server Error')) {
+                    //     throw new Error('Internal Server Error');
+                    // }
+
+                    if (responseItem.includes('event: ping')) {
+                        callback({ 'status': 'ping', 'data': '' })
+                        return reader.read().then(processText);
+                    }
+                    var items = responseItem.split('\n\n');
+                    items.pop();
+                    //responseItem=items.pop();
+
+                    const last1 = items.pop();
+                    const last2 = items.pop();
+                    let pid = '', cid = '';
+
+                    if (!last1) {
+                        callback({ 'status': 'error', 'data': responseItem });
+                        return;
+                        // throw new Error(responseItem);
+                    }
+
+                    if (!last1.includes('data: [DONE]')) {
+                        let temp = JSON.parse(last1.slice(6));
+                        let answer = temp.message.content.parts[0];
+                        pid = temp.message.id;
+                        cid = temp.conversation_id;
+                        callback({ 'status': 'loading', 'data': answer, pid: pid, cid: cid });
+                    } else {
+                        if (last2) {
+                            let temp = JSON.parse(last2.slice(6));
+                            let answer = temp.message.content.parts[0];
+                            pid = temp.message.id;
+                            cid = temp.conversation_id;
+                            callback({ 'status': 'loading', 'data': answer, pid: pid, cid: cid });
+                        }
+                        setTimeout(() => { callback({ 'status': 'done', 'data': '' }) }, 100)
+                        return;
+                    }
+
+                    return reader.read().then(processText)
+                })
+            },
+            ontimeout: function (e) {
+                throw new Error("generateAnswer error: ontimeout");
+            },
+            onerror: function (error) {
+                throw new Error("generateAnswer error: " + error);
+            }
+        })
+    }
+
+    class ChatWebSocket {
+        constructor() {
+            var ws = new WebSocket("ws://127.0.0.1:7272/");
+            this.sendMsg = function (to_client_id, content) {
+                let msg = {
+                    "type": "say",
+                    "to_client_id": to_client_id,
+                    "to_client_name": "user",
+                    "content": content
+                };
+                ws.send(JSON.stringify(msg));
+            };
+            this.connect = () => {
+                ws.onopen = () => {
+                    var login_data = { "type": "login", "client_name": "admin", "room_id": "1", "is_admin": true };
+                    ws.send(JSON.stringify(login_data));
+                };
+                ws.onmessage = (e) => {
+                    var data = JSON.parse(e.data);
+                    switch (data['type']) {
+                        case 'ping':
+                            ws.send('{"type":"pong"}');
+                            break;
+                        case 'login':
+                            console.log(data['client_id'] + "登录成功");
+                            break;
+                        case 'say':
+                            if (data['from_client_name'] == 'user') {
+                                generateAnswer(data['content'], ans => {
+                                    //console.log(ans);
+                                    this.sendMsg(data['from_client_id'], JSON.stringify(ans));
+                                }).then().catch(err => {
+                                    console.error(err);
+                                    this.sendMsg(data['from_client_id'], JSON.stringify({
+                                        'status': 'error',
+                                        'data': '' + err
+                                    }));
+                                });
+                            }
+                            break;
+                        case 'logout':
+                            console.log(data['from_client_id'] + "退出");
+                    }
+                };
+                ws.onclose = function () {
+                    console.log("连接关闭");
+                    setTimeout(() => { this.connect(); }, 2000);
+                };
+                ws.onerror = function (e) {
+                    console.error('连接出错', e);
+                    setTimeout(() => { this.connect(); }, 2000);
+                };
+            };
+            return this;
+        }
+    }
+
+    async function main() {
+        var ws = new ChatWebSocket();
+        ws.connect();
+    }
+
+    main().then();
+})();
